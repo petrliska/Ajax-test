17bc6d6f65165bed25e70f909b631e217eee753e
petr liska <petr_liska@email.cz>
2013-09-09 13:12:08 +0200

    Výstup celou větou

--- time.php
+++ time.php
@@ -1,7 +1,10 @@
 <?php
 
-$t = Date("H:i:s", Time());
+$t = Time();
+$s = $t % 60;
+$m = ($t / 60) % 60;
+$h = ($t / 3600) % 24;
 
-echo $t;
+echo "Právě je ".$h." hodin, ".$m." minut a ".$s." sekund!";
 
 ?>
