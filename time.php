7f72b7322ba4f3fe1d3d4971b60db19e5ffbda1e
petr liska <petr_liska@email.cz>
2013-09-11 13:31:59 +0200

    přidán refresh

--- time.php
+++ time.php
@@ -1,4 +1,5 @@
-<?php
+﻿<?php
+    //header("refresh: 3;");	//refresh pomocí PHP nefunguje v načtené HTML stránce
     $datum = StrFTime("Právě je %H hodin, %M minut a %S sekund", Time());
     echo($datum);
 ?>
